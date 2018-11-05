c11ce607abd23955baf188b65bd2b572ef75edcd
princesso <egbunaoluebube@gmail.com>
2018-11-05 16:39:36 +0100

    update tests

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const functions = require('./functions')
 test('expect 3+3 to be 6', ()=>{
   expect(functions.add(3,3)).toBe(6)
 });
+
+test('isNull to be null', ()=>{
+  expect(functions.isNull()).toBeNull()
+});
